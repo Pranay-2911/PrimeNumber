9b1bde87e3ddd298d462343da56fe6e4e34de4e4
Pranay Raut <pranayraut129@gmail.com>
2024-09-26 01:42:37 +0530

    Add project files.

---
+++ PrimeNumber/Models/CheckingPrime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber.Models
+{
+    internal class CheckingPrime
+    {
+        public static bool ToCheckPrime(int number)
+        {
+            if (number <= 1) return false;
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using PrimeNumber.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(CheckingPrime.ToCheckPrime(number) ? $"{number} is Prime" : $"{number} is not a Prime");
+
+        }
+    }
+}
